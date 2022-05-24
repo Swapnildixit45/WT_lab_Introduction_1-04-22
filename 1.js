1f45c94404a09f166e879af22cf9ff02b9e06ee5
Swapnildixit45 <57413050+Swapnildixit45@users.noreply.github.com>
2022-05-24 10:18:58 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,20 @@
+function bubbleSort(array) {
+    var done = false;
+    while (!done) {
+      done = true;
+      for (var i = 1; i < array.length; i += 1) {
+        if (array[i - 1] > array[i]) {
+          done = false;
+          var tmp = array[i - 1];
+          array[i - 1] = array[i];
+          array[i] = tmp;
+        }
+      }
+    }
+  
+    return array;
+  }
+  
+  var arr = [4,7,7,8,2,1,-2];
+  bubbleSort(arr);
+  console.log(arr);
---
+++ 4c.js
@@ -0,0 +1,6 @@
+const arr = ['First item', 'Second item', 'Third item'];
+
+const arrLength = arr.unshift('Urgent item 1', 'Urgent item 2');
+
+console.log(arrLength);  
+console.log(arr); 
